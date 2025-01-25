d5cdca9cc4c7c75b9889d5f269110c102c9dacfb
Samip Paudel <sss.detonator@gmail.com>
2025-01-25 00:32:50 -0500

    added lab1d.py for lab1

---
+++ lab1d.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''Description: This program will output "hello world" to the screen.'''
+
+# Last part Lab1 
+# ops445
+
+x = 10
+y = 2
+z = 5
+
+result = x + y * z
+
+print(str(x) + ' + ' + str(y) + ' * ' + str(z) + ' = ' + str(result))
